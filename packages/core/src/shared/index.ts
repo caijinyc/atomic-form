abaef7739871bee29ac6ef205ab814557386e613
jin <me@caijin.tech>
2022-06-19 21:47:48 +0800

    🔥 remove lazy cb

--- packages/core/src/shared/index.ts
+++ packages/core/src/shared/index.ts
@@ -2,14 +2,14 @@ import { nextTick } from '@atomic-form/shared'
 
 export { buildGetAllChildren, buildSetState, buildNode, triggerModified, generatePathString } from './internal'
 
-export const buildLazyCallback = (originCb: (...args: any) => any) => {
-  let count = 0
-  return async(state?: any) => {
-    const currentCount = count++
-    await nextTick()
-    if (currentCount === count - 1) {
-      count = 0
-      originCb(state)
-    }
-  }
-}
+// export const buildLazyCallback = (originCb: (...args: any) => any) => {
+//   let count = 0
+//   return async(state?: any) => {
+//     const currentCount = count++
+//     await nextTick()
+//     if (currentCount === count - 1) {
+//       count = 0
+//       originCb(state)
+//     }
+//   }
+// }
